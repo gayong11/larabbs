fb05a38f263251aa4a4b99392d9eff0b9a5be5d2
vagrant <vagrant@homestead>
2019-10-16 09:20:51 +0000

    first

---
+++ app/Http/Controllers/Api/CaptchasController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Gregwar\Captcha\PhraseBuilder;
+use Illuminate\Http\Request;
+use Gregwar\Captcha\CaptchaBuilder;
+use App\Http\Requests\Api\CaptchaRequest;
+
+class CaptchasController extends Controller
+{
+    public function store(CaptchaRequest $request)
+    {
+        $key = 'captcha-'.str_random(15);
+        $phone = $request->phone;
+
+        $phraseBuilder = new PhraseBuilder(4, '0123456789');
+        $captchaBuilder = new CaptchaBuilder(null, $phraseBuilder);
+        $captcha = $captchaBuilder->build();
+        $expiredAt = now()->addMinutes(2);
+        \Cache::put($key, ['phone' => $phone, 'code' => $captcha->getPhrase()], $expiredAt);
+
+        $result = [
+            'captcha_key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+            'captcha_image_content' => $captcha->inline(),
+        ];
+
+        return $this->response->array($result)->setStatusCode(201);
+    }
+}
---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        Registered::class => [
+            SendEmailVerificationNotification::class,
+        ],
+        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+            // add your listeners (aka providers) here
+            'SocialiteProviders\Weixin\WeixinExtendSocialite@handle'
+        ],
+        \Illuminate\Auth\Events\Verified::class => [
+            \App\Listeners\EmailVerified::class,
+        ],
+        'eloquent.created: Illuminate\Notifications\DatabaseNotification' => [
+            'App\Listeners\PushNotification',
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+    }
+}
